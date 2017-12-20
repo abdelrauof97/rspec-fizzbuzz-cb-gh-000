bbf39fe2777fdc1add195d67243149f6571ee81f
abdelrauof mohammed <abdelrauof97@gmail.com>
2017-12-20 01:50:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
- 
-fizzbuzz(3) 
-fizzbuzz(5) 
-fizzbuzz()  
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz()
 end
